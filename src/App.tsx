64085171e74712d08cb4dec413c13426d398054e
avkotau <kotov.alexander1@mail.ru>
2023-03-30 17:59:26 +0200

    next task BrouserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import MyPosts from "./components/Profile/MyPosts/MyPosts";
+import Dialogues from "./components/Dialogues/Dialogues";
+import s from './components/Dialogues/Dialogues.module.css'
 
 function App() {
     return (
         <div className='app'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className={s.dialogs}>
+                {/*<Dialogues/>*/}
+                <Profile/>
+            </div>
 
         </div>
     );
---
+++ src/components/Dialogues/Dialogues.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dialogues = () => {
+    return (
+        <div>
+            Dialogues
+        </div>
+    );
+};
+
+export default Dialogues;
