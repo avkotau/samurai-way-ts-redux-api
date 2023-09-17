e0f147c31b195dd811f946f2a7c908876a556b8e
avkotau <kotov.alexander1@mail.ru>
2023-09-17 15:55:28 +0200

    feat: createSelect with library "reselect" ( for study - this logic is not useful in this project )

--- src/store/users-selectors.ts
+++ src/store/users-selectors.ts
@@ -1,8 +1,17 @@
 import { AppStateType } from "store/redux-store";
+import { createSelector } from "reselect";
 
-export const getUsersSelector = (state: AppStateType) => {
+const getUsers = (state: AppStateType) => {
     return state.usersPage.users
 };
+// This example how to work reselect ( getUsers - this dependence
+// if anyone has changed then the selector is called. getUsersSelector set in mapStateToProps
+export const getUsersSelector = createSelector(
+    getUsers,
+    (users) => {
+        //fake filter
+        return users.filter(() => true)
+    });
 export const getPageSizeSelector = (state: AppStateType) => {
     return state.usersPage.pageSize
 };
