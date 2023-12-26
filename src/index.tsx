3540648261e3c2a53135e867b6f079d17e01fbf2
avkotau <kotov.alexander1@mail.ru>
2023-12-26 21:53:21 +0100

    fix: update HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "store/redux-store";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App
                 state={store.getState()}
                 dispatch={store.dispatch.bind(store)}
             />
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
