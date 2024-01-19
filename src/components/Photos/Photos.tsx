c37b787274e82bcbed90e57a0002abf992328467
avkotau <kotov.alexander1@mail.ru>
2024-01-19 20:12:46 +0100

    feat: add links for pictures and styles

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -1,21 +1,20 @@
-import photo from './../../assets/Photos/beautiful_placeholder.png'
-import s from './Photos.css'
+import s from 'components/Photos/Photos.module.css'
 export const Photos = () => {
     return (
         <div className={s.photos}>
             <div className={s.photosWrapper}>
-                <img src={photo} alt="photo1" />
-                <img src={photo} alt="photo2" />
-                <img src={photo} alt="photo3" />
-                <img src={photo} alt="photo4" />
-                <img src={photo} alt="photo5" />
-                <img src={photo} alt="photo6" />
-                <img src={photo} alt="photo7" />
-                <img src={photo} alt="photo8" />
-                <img src={photo} alt="photo9" />
-                <img src={photo} alt="photo10" />
-                <img src={photo} alt="photo11" />
-                <img src={photo} alt="photo12" />
+                <img src={'https://img.freepik.com/free-photo/portrait-of-satisfied-blonde-attractive-delighted-female-indicates-ok-sign-keeps-eyes-shut-being-in-good-mood-happy-to-have-weekends-isolated-over-pink_273609-3874.jpg?w=1380&t=st=1705690981~exp=1705691581~hmac=9d6fdf9b8b1de75f3a2befaff1b2e0a11b0cf73c702d7a25944adb465cb89d7e'} alt="photo1" />
+                <img src={'https://img.freepik.com/free-photo/girl-holding-a-glass-with-champagne-stands-in-a-foreground_8353-11576.jpg?w=740&t=st=1705691091~exp=1705691691~hmac=122e459f96909079f37003d84be83713a854d102ac110a9bb70cc79209d0e0fd'} alt="photo2" />
+                <img src={'https://img.freepik.com/free-photo/young-pretty-smiling-happy-woman-in-red-mittens-and-hat-wearing-knitted-sweater-walking-in-park-in-snow-warm-clothes-having-fun-waving-long-hair_285396-5174.jpg?w=1380&t=st=1705691074~exp=1705691674~hmac=adef9359e5f43c960249408f2f07e7789d29e825d2c803eb0cac0234dac048e2'} alt="photo3" />
+                <img src={'https://img.freepik.com/free-photo/young-handsome-man-posing_144627-28077.jpg?w=740&t=st=1705691067~exp=1705691667~hmac=68fc1a49f0c836144fdd6b180d243b04e6392d88692f6f35a4636c69c9b3e230'} alt="photo4" />
+                <img src={'https://img.freepik.com/free-photo/young-woman-portrait-outdoor-in-black-wear_624325-3417.jpg?w=740&t=st=1705691093~exp=1705691693~hmac=8a6002ef0282d4099c831580377201d94ab89bbd633de93992211a8cf93135a0'} alt="photo5" />
+                <img src={'https://img.freepik.com/free-photo/emotions-and-people-concept-headshot-of-serious-looking-handsome-man-with-beard-looking-confident-and-determined_1258-26730.jpg?w=1380&t=st=1705691280~exp=1705691880~hmac=989624fcf7501959afb524eb7265012ef1a87d00fe05613c9bd4234c6bacf6b4'} alt="photo6" />
+                <img src={'https://img.freepik.com/free-photo/woman-receives-a-gift-from-her-boyfriend_155003-11570.jpg?w=1380&t=st=1705691407~exp=1705692007~hmac=bc595f13281c048a1894f395b76ba67b59482c7f6ef99cba944fb841f06871d1'} alt="photo7" />
+                <img src={'https://img.freepik.com/free-photo/woman-with-blond-short-hair-tanned-smiling-cute-and-gazing-at-camera-with-friendly_176420-44610.jpg?w=1380&t=st=1705691366~exp=1705691966~hmac=89571f33f8c52749625a036c0742bfc3b8b4ff305afb51d98fe1348cfca4e4ae'} alt="photo8" />
+                <img src={'https://img.freepik.com/free-photo/image-of-hesitant-unshaven-european-man-with-thick-beard-holds-chin-purses-lips-with-clueless-expressions_273609-17361.jpg?w=1380&t=st=1705691309~exp=1705691909~hmac=a45449adfc0f0da21e9092d1f20e2b1d44ada2e33a85ccf7d1bdd375932f7ea2'} alt="photo9" />
+                <img src={'https://img.freepik.com/premium-photo/set-of-handsome-emotional-man_146377-5582.jpg?w=826'} alt="photo10" />
+                <img src={'https://img.freepik.com/free-photo/portrait-of-cute-couple-having-fun-in-park-pretty-girl-with-long-curly-hair-is-riding-on-back-of-handsome-guy-they-wear-sunglasses-and-smile-to-camera_197531-24824.jpg?w=1380&t=st=1705691099~exp=1705691699~hmac=4ba85b79ff120c164f763a238dd77754198e1420aa28c605e9179212a6c2bbdc'} alt="photo11" />
+                <img src={'https://img.freepik.com/free-photo/human-face-expressions-and-emotions-positive-joyful-young-beautiful-female-with-fair-straight-hair-in-casual-clothing_176420-15188.jpg?w=1380&t=st=1705691369~exp=1705691969~hmac=bff71b712ba659e755a0e5a55491e57e0e67bb32d5fee05ca2cdb08961eb6f8a'} alt="photo12" />
             </div>
         </div>
     )
